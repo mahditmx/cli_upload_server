a8c849004051ac3c239aea9dd6a718fc43f3addc
mahdi <mahditizrokhasmakhi@gmail.com>
2024-07-01 14:24:36 +0330

    add 0.0.5 deb file

--- app.py
+++ app.py
@@ -197,7 +197,8 @@ def main():
 @app.route('/downloads')
 def downloads():
     return """<h1>Download .deb</h1>Cloud database - Cbase 
-    <br><br> <a href='/download/deb/0.0.4' >cbase-0.0.4.deb</a> for linux 8.2MB 62eba599bfe2acfd15987b7b8ca665af1cd5d8082c9b1e44fb33044c32f98adf   - Last version
+    <br><br> <a href='/download/deb/0.0.5' >cbase-0.0.5.deb</a> for linux 8.2MB 2a1d6becd2b7a600bc7b0c438da6fa09434efd28c2fd4bb2160e2589467d6092   - Last version
+    <br> <a href='/download/deb/0.0.4' >cbase-0.0.4.deb</a> for linux 8.2MB 62eba599bfe2acfd15987b7b8ca665af1cd5d8082c9b1e44fb33044c32f98adf
     <br> <a href='/download/deb/0.0.3' >cbase-0.0.3.deb</a> for linux 8.2MB 3ec0df369cfd3ae4258a62a06835d325be8b7f24212ed5de9482f99ccca050ba
     <br> <a href='/download/deb/0.0.2' >cbase-0.0.2.deb</a> for linux 8.2MB 8641ef435e65ab9e862200a959595c2a0e6727c9b479a8cbe56389d39cb2d734
     
